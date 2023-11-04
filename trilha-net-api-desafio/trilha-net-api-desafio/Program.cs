b813c578dfbdc9d3c4bf71b2de72e414d95a8dc7
Leandro Gomes - E. Escola de T.I - BNE <cesarsilva@bne.com.br>
2023-11-04 18:45:16 -0300

    CHANGE: feito a implementação do codigo pendente

--- trilha-net-api-desafio/trilha-net-api-desafio/Program.cs
+++ trilha-net-api-desafio/trilha-net-api-desafio/Program.cs
@@ -6,11 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
+// Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
